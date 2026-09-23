985ea8408960fe4338ddbd5d28fac9eac649e8dc
Russell Keith-Magee <russell@keith-magee.com>
2026-09-23 08:53:10 +0800

    Only explicitly set the system logger on Python 3.14+

--- {{ cookiecutter.format }}/{{ cookiecutter.class_name }}/main.m
+++ {{ cookiecutter.format }}/{{ cookiecutter.class_name }}/main.m
@@ -63,8 +63,10 @@ int main(int argc, char *argv[]) {
         config.install_signal_handlers = 1;
         // Isolated apps need to set the full PYTHONPATH manually.
         config.module_search_paths_set = 1;
+        {% if cookiecutter.python_version|minor_version >= 14 and not cookiecutter.console_app -%}
         // Enable the use of the system logger
         config.use_system_logger = 1;
+        {% endif -%}
         // For debugging - enable verbose mode.
         // config.verbose = 1;
 
